13faaeba5bc15d6a5442a050ed15c9e8bd149cff
davebulner <IT19214580@my.sliit.lk>
2022-04-07 17:48:35 +0530

    data retrieve

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,6 +7,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lunch_partner/home.dart';
 import 'package:lunch_partner/registration.dart';
+
+import 'model/userModel.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -15,6 +18,25 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+
+
 File? image;
 
   Future pickImage() async {
@@ -29,12 +51,24 @@ File? image;
  }
   }
 
+Future pickImageCamera() async {
+  try {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image == null) return;
+
+    final imageTemporary = File(image.path);
+    setState(() => this.image = imageTemporary);
+  } on PlatformException catch (e){
+    print('Failed to pick image: $e');
+  }
+}
+
   @override
   Widget build(BuildContext context) {
 
     //password field
 
-    final loginButton = Material(
+    final imageButton = Material(
       elevation: 5,
       borderRadius: BorderRadius.circular(30),
       color: Colors.redAccent,
@@ -46,7 +80,23 @@ File? image;
             "Image Upload",
             textAlign: TextAlign.center,
             style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+                fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
+    final cameraButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(5, 3, 5, 3),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () => pickImageCamera(),
+          child: Text(
+            "use camera",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
           )),
     );
 
@@ -67,7 +117,34 @@ File? image;
                    width: 160,
                    height: 160,
                    fit: BoxFit.cover) : FlutterLogo(size: 160),
-                    loginButton,
+                    SizedBox(height: 16),
+                    imageButton,
+                    SizedBox(height: 16),
+                    cameraButton,
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text("User Name: ${loggedInUser.firstName} ${loggedInUser.secondName}",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        )),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text("Email: ${loggedInUser.email}",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        )),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text("Phone Number: ${loggedInUser.phoneNum}",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        )),
 
                   ],
                 ),
