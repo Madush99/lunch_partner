3a55c2798a1a7b4410c96cb8fba3410c3f1e1535
davebulner <IT19214580@my.sliit.lk>
2022-04-11 18:49:02 +0530

    text message and delete

--- lib/profile.dart
+++ lib/profile.dart
@@ -3,14 +3,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lunch_partner/home.dart';
+import 'package:lunch_partner/notification.dart';
 import 'package:lunch_partner/registration.dart';
+import 'package:lunch_partner/updateProfile.dart';
+
+
 
 import 'model/userModel.dart';
+import 'note2.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends StatefulWidget{
+
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
@@ -36,51 +43,48 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
 
-
-File? image;
+  File? image;
 
   Future pickImage() async {
- try {
-   final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-   if (image == null) return;
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
 
-   final imageTemporary = File(image.path);
-   setState(() => this.image = imageTemporary);
- } on PlatformException catch (e){
-   print('Failed to pick image: $e');
- }
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e){
+      print('Failed to pick image: $e');
+    }
   }
 
-Future pickImageCamera() async {
-  try {
-    final image = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (image == null) return;
+  Future pickImageCamera() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (image == null) return;
 
-    final imageTemporary = File(image.path);
-    setState(() => this.image = imageTemporary);
-  } on PlatformException catch (e){
-    print('Failed to pick image: $e');
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e){
+      print('Failed to pick image: $e');
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
-
-    //password field
 
     final imageButton = Material(
       elevation: 5,
       borderRadius: BorderRadius.circular(30),
       color: Colors.redAccent,
       child: MaterialButton(
-          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+          minWidth: MediaQuery.of(context).size.height,
           onPressed: () => pickImage(),
           child: Text(
             "Image Upload",
             textAlign: TextAlign.center,
             style: TextStyle(
-                fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
+                fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
           )),
     );
 
@@ -89,73 +93,235 @@ Future pickImageCamera() async {
       borderRadius: BorderRadius.circular(30),
       color: Colors.redAccent,
       child: MaterialButton(
-          padding: EdgeInsets.fromLTRB(5, 3, 5, 3),
+          padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
           minWidth: MediaQuery.of(context).size.width,
           onPressed: () => pickImageCamera(),
           child: Text(
             "use camera",
             textAlign: TextAlign.center,
             style: TextStyle(
-                fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
+                fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
           )),
     );
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                   image!= null ? Image.file(image!,
-                   width: 160,
-                   height: 160,
-                   fit: BoxFit.cover) : FlutterLogo(size: 160),
-                    SizedBox(height: 16),
-                    imageButton,
-                    SizedBox(height: 16),
-                    cameraButton,
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text("User Name: ${loggedInUser.firstName} ${loggedInUser.secondName}",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                        )),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text("Email: ${loggedInUser.email}",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                        )),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text("Phone Number: ${loggedInUser.phoneNum}",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                        )),
-
-                  ],
-                ),
-              ),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(4, 9, 35, 1),
+                Color.fromRGBO(220, 12, 12, 1.0),
+              ],
+              begin: FractionalOffset.bottomCenter,
+              end: FractionalOffset.topCenter,
             ),
           ),
         ),
-      ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 73),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        AntDesign.arrowleft,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        AntDesign.logout,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'My\nProfile',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontFamily: 'Nisebuschgardens',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  Container(
+                    height: height * 0.70,
+
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: innerHeight * 0.70,
+                                width: innerWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    SizedBox(height: 50),
+                                    imageButton,
+                                    SizedBox(height: 10),
+                                    cameraButton,
+                                    SizedBox(height: 40),
+                                    Text(
+                                      '${loggedInUser.firstName} ${loggedInUser.secondName}',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(39, 105, 171, 1),
+                                        fontFamily: 'Nunito',
+                                        fontSize: 37,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Email',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 19,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              "${loggedInUser.email}",
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 14,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                            vertical: 8,
+                                          ),
+                                          child: Container(
+                                            height: 50,
+                                            width: 3,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                              BorderRadius.circular(100),
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Phone Number',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 19,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              "${loggedInUser.phoneNum}",
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    39, 105, 171, 1),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 14,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 110,
+                              right: 20,
+                              child: Icon(
+                                AntDesign.setting,
+                                color: Colors.grey[700],
+                                size: 30,
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Container(
+                                  child: image!= null ? Image.file(image!, width: 200, height: 120,
+                                      ) : FlutterLogo(size: 70),
+                                ),
+                              ),
+
+                            ),
+
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  // SizedBox(height: 10),
+                  // imageButton,
+                  // SizedBox(height: 10),
+                  // cameraButton,
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ActionChip(
+                      label: Text("Profile Update"),
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) => UpdateProfileScreen()));
+                      }),
+
+                  ActionChip(
+                      label: Text("Send SMS"),
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) => Not2Page(title: 'notification',)));
+                      }),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
-
-  //login function
-
 }
